13ec4c30e0910d8a366836e10d243e560c2df84d
blechnapp <kay.kaestner@me.com>
2025-11-01 13:33:40 +0100

    Refactor OSVCustomMetaServiceProvider class

--- src/Providers/OSVCustomMetaServiceProvider.php
+++ src/Providers/OSVCustomMetaServiceProvider.php
@@ -1,27 +1,33 @@
 <?php
+declare(strict_types=1);
 
 namespace OSVCustomMeta\Providers;
 
 use Plenty\Plugin\ServiceProvider;
 use Plenty\Plugin\Templates\TemplateContainer;
+use Plenty\Plugin\Log\Loggable;
 
-class OSVCustomMetaServiceProvider extends ServiceProvider
+final class OSVCustomMetaServiceProvider extends ServiceProvider
 {
+    use Loggable;
+
     public function register(): void
     {
-        // Keine Registrierung nötig
+        // nichts nötig
     }
 
     public function boot(TemplateContainer $container): void
     {
-        // A) Wrapper gezielt überschreiben:
+        // Sichtbarer Logeintrag zum Nachweis, dass der Provider läuft
+        $this->getLogger(__CLASS__)->info('OSVCustomMeta booted', []);
+
+        // Beispiel: SingleItemWrapper überschreiben
         $container->set(
             'Ceres::Item.SingleItemWrapper',
             'OSVCustomMeta::Item.SingleItemWrapper'
         );
 
-        // B) Optional: Meta-Partial direkt überschreiben
-        // (auskommentieren, wenn du nur mit dem Wrapper arbeiten willst)
+        // Optional (zum Hard-Test): Meta-Partial überschreiben
         // $container->set(
         //     'Ceres::PageDesign.Partials.PageMetaData',
         //     'OSVCustomMeta::PageDesign.Partials.PageMetaData'
