cc49fe5d40cc32cc7293345f44220a78835d9e1d
blechnapp <kay.kaestner@me.com>
2025-11-06 14:29:42 +0100

    Create OSVServiceProvider.php

---
+++ src/Providers/OSVServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace OSVCustomMeta\Providers;
+
+use Plenty\Plugin\ServiceProvider;
+
+class OSVServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        // Keine Registrierung nötig – Provider muss nur existieren,
+        // damit das Plugin sauber bereitgestellt wird.
+    }
+}
